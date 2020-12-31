2bef5d3321a3152afc44c1c21a65187203c23d24
Ed Addario <eaddario@hotmail.com>
2020-12-31 00:34:01 +0000

    add startup configuration parameters

--- cmd/erised/main.go
+++ cmd/erised/main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"path/filepath"
+	"strconv"
 	"time"
 )
 
@@ -11,22 +16,41 @@ type server struct {
 	cfg  *http.Server
 }
 
-func newServer() *server {
+func newServer(port, read, write, idle int) *server {
 	s := &server{}
 	s.mux = &http.ServeMux{}
 	s.cfg = &http.Server{
-		Addr:         ":8080",
+		Addr:         ":" + strconv.Itoa(port),
 		Handler:      s.mux,
-		ReadTimeout:  5 * time.Second,
-		WriteTimeout: 10 * time.Second,
-		IdleTimeout:  120 * time.Second,
+		ReadTimeout:  time.Duration(read) * time.Second,
+		WriteTimeout: time.Duration(write) * time.Second,
+		IdleTimeout:  time.Duration(idle) * time.Second,
 	}
 	s.routes()
+
+	log.Printf("Server configured to listen on port %s. Timeouts: %s (read) %s (write) %s (idle)\n",
+		s.cfg.Addr, s.cfg.ReadTimeout.String(), s.cfg.WriteTimeout.String(), s.cfg.IdleTimeout.String())
 
 	return s
 }
 
+func setupFlags(f *flag.FlagSet) {
+	f.Usage = func() {
+		_, _ = fmt.Printf("%s: a simple http server to test arbitrary responses. Usage example at https://github.com/EAddario/erised\n", filepath.Base(os.Args[0]))
+		fmt.Println("\nParameters:")
+		flag.PrintDefaults()
+	}
+}
+
 func main()  {
-	srv := newServer()
+	pt := flag.Int("port", 8080, "port to listen")
+	rt := flag.Int("read", 5, "read timeout in seconds")
+	wt := flag.Int("write", 10, "write timeout in seconds")
+	it := flag.Int("idle", 120, "idle timeout in seconds")
+
+	setupFlags(flag.CommandLine)
+	flag.Parse()
+
+	srv := newServer(*pt, *rt, *wt, *it)
 	log.Fatal(srv.cfg.ListenAndServe())
 }
