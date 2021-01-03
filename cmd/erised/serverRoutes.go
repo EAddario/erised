b79a8e1245f4ffdefc993e4f49ba8ff9b0ad0e5f
Ed Addario <eaddario@hotmail.com>
2021-01-03 00:07:41 +0000

    Add GZIP encoding

--- cmd/erised/serverRoutes.go
+++ cmd/erised/serverRoutes.go
@@ -21,17 +21,13 @@ func (s *server) handleLanding() http.HandlerFunc {
 		log.Printf("%s from %s - %s %s%s",
 			req.Proto, req.RemoteAddr, req.Method, req.Host, req.RequestURI)
 
-		res.Header().Set("Content-Type", "text/plain; charset=utf-8")
 		delay := time.Duration(0)
+		enc, ct, ce := encoding(req.Header.Get("X-Erised-Content-Type"))
 
+		res.Header().Set("Content-Type", ct)
+		res.Header().Set("Content-Encoding", ce)
 		if rd, err := strconv.Atoi(req.Header.Get("X-Erised-Response-Delay")); err == nil {
 			delay = time.Duration(rd) * time.Millisecond
-		}
-		if ct := req.Header.Get("X-Erised-Content-Type"); ct != "" {
-			res.Header().Set("Content-Type", ct)
-		}
-		if te := req.Header.Get("X-Erised-Transfer-Encoding"); te != "" {
-			res.Header().Set("Transfer-Encoding", te)
 		}
 		sc := httpStatusCode(req.Header.Get("X-Erised-Status-Code"))
 		if sc >= 300 && sc < 310 {
@@ -42,7 +38,7 @@ func (s *server) handleLanding() http.HandlerFunc {
 
 		data := req.Header.Get("X-Erised-Data")
 
-		s.respond(res, encodingTEXT, delay, data)
+		s.respond(res, enc, delay, data)
 	}
 }
 
@@ -51,7 +47,7 @@ func (s *server) handleHeaders() http.HandlerFunc {
 		log.Printf("%s from %s - %s %s%s",
 			req.Proto, req.RemoteAddr, req.Method, req.Host, req.RequestURI)
 
-		res.Header().Set("Content-Type", "application/json; charset=utf-8")
+		res.Header().Set("Content-Type", "application/json")
 
 		data := "{"
 		for k, v := range req.Header {
@@ -68,7 +64,7 @@ func (s *server) handleHeaders() http.HandlerFunc {
 		data += "\"Host\":\"" + req.Host + "\""
 		data += "}"
 
-		s.respond(res, encodingTEXT, 0, data)
+		s.respond(res, encodingJSON, 0, data)
 	}
 }
 
@@ -77,13 +73,13 @@ func (s *server) handleIP() http.HandlerFunc {
 		log.Printf("%s from %s - %s %s%s",
 			req.Proto, req.RemoteAddr, req.Method, req.Host, req.RequestURI)
 
-		res.Header().Set("Content-Type", "application/json; charset=utf-8")
+		res.Header().Set("Content-Type", "application/json")
 
 		data := "{"
 		data += "\"Client IP\":\"" + req.RemoteAddr + "\""
 		data += "}"
 
-		s.respond(res, encodingTEXT, 0, data)
+		s.respond(res, encodingJSON, 0, data)
 	}
 }
 
@@ -92,7 +88,7 @@ func (s *server) handleInfo() http.HandlerFunc {
 		log.Printf("%s from %s - %s %s%s",
 			req.Proto, req.RemoteAddr, req.Method, req.Host, req.RequestURI)
 
-		res.Header().Set("Content-Type", "application/json; charset=utf-8")
+		res.Header().Set("Content-Type", "application/json")
 
 		data := "{"
 		data += "\"Host\":\"" + req.Host + "\","
@@ -101,6 +97,6 @@ func (s *server) handleInfo() http.HandlerFunc {
 		data += "\"Request URI\":\"" + req.RequestURI + "\""
 		data += "}"
 
-		s.respond(res, encodingTEXT, 0, data)
+		s.respond(res, encodingJSON, 0, data)
 	}
 }
